56f83c73b5e1928018a3dce45b73d44f0da0f8be
Sagun-Nakarmi <sagun.nakarmi@csnet.net.au>
2025-06-05 06:49:21 +0000

    Add Jest configuration, package.json, and a basic test

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["js", "json"],
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"]
+};
